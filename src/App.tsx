796fa82611b37fecc558c7c760792a057fdcc931
Anish Sharma <sharmaanish023@gmail.com>
2024-02-13 22:43:29 +0545

    Add side bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold ">
+      <Sidebar />
+    </h1>
+  );
 }
 
 export default App;
